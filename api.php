897dffce329827a088c31287d3b6fbb1cc09f250
Dakota <unixinspace@gmail.com>
2018-09-07 10:41:28 -0500

    Api spaghetti

--- api.php
+++ api.php
@@ -1,2 +1,15 @@
 <?php 
 
+// Str: /point/sub
+$point = $_GET['point'];
+$sub = $_GET['sub'];
+$key = $_POST['key'];
+
+if(null($point) || null($sub)){
+    die("Invalid Request");
+}
+
+
+if($point == "read"){
+    echo $sub;
+}
