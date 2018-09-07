d1f22db2308ba79048b4006476d25f3201bea05d
Dakota <unixinspace@gmail.com>
2018-09-07 11:23:42 -0500

    fleshed out drink{}, added mixy{} (mixologist)

--- main.php
+++ main.php
@@ -10,10 +10,28 @@ class drink{
     private $submitterid;
     public $submittername;
     
+    function __construct($id){
+       //SQL for getting id, submitter name
+        $this->id = $id;
+        $this->ups = 6;
+        $this->downs = 2;
+        $this->score = $this->ups - $this->downs;  
+        $this->submitterid = 1;
+        $this->submittername = "Joe Blow";
+        
+    }
+
+    //tally called on refresh, after vote, etc.
     function tally($id){
         //SQL for $up's
         $this->ups = 6;
         //SQL for $down's
         $this->downs = 2;
         $this->score = $this->ups - $this->downs;
+    }
+}
 
+class mixy{
+
+
+}
